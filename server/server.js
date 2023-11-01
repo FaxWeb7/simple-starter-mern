b6229385a55f0ccd411987792f3661ca5430343f
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2023-11-01 21:24:33 +0300

    remove chalk

--- server/server.js
+++ server/server.js
@@ -5,30 +5,22 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const nameRoutes = require("./routes/name-routes");
 const { createPath, handleError } = require("./helpers/helper");
-const chalk = require("chalk");
-const successMsg = chalk.bgKeyword("green").white.bold;
-const errorMsg = chalk.bgKeyword("white").red;
-const reqMsg = chalk.bgKeyword("purple").cyan.bold.underline;
 
 const app = express();
 app.set("view engine");
 
 /*mongoose  
   .connect(process.env.MONGO_URL)  
-  .then((res) => console.log(successMsg('Connected to DB'))) 
-  .catch((error) => console.log(errorMsg(error))); */
+  .then((res) => console.log('Connected to DB')) 
+  .catch((error) => console.log(error)); */
 
 app.listen(process.env.PORT, (error) => {
   error
-    ? console.log(errorMsg(error))
-    : console.log(successMsg(`Listening port ${process.env.PORT}`));
+    ? console.log(error)
+    : console.log(`Listening port ${process.env.PORT}`);
 });
 
-app.use(
-  morgan(
-    reqMsg(":method :url :status :res[content-length] - :response-time ms")
-  )
-);
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
 
